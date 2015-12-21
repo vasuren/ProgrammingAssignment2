3a4d6c6b4a6d7f1f51782a8e25ea43f3c0899618
vasuren <vasu_suren@yahoo.co.in>
2015-12-21 08:22:52 +0530

    Completed Cache Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The program is written to make a special matrix and cache its inverse
+## if the cache is not available, there is a special function written
+## to calculate it as necessary
 
-## Write a short comment describing this function
+## Creating a special matrix and caches its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function() {
+     set.seed(1)                                   #setting an intial seed
+     x = matrix(c(sample(1:9)),nrow = 3, ncol = 3) #creating a random matrix
+     xinv <<- solve(x)                             # Caching inverted matrix
+          
 }
 
 
-## Write a short comment describing this function
+## Checks for inverted matrix in cache and returns if it exists
+## else calculates it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     set.seed(1)
+     x = matrix(c(sample(1:9)),nrow = 3, ncol = 3)
+     m <- get(xinv)
+     if(!is.null(m)){
+          message("getting cached matrix")
+          return(m) ## Return a matrix that is the inverse of 'x' from cache
+     }
+        matr <- x
+        m <- solve(matr)
 }
